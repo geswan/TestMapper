3221b2eb8a81491ea1b8a4b3e96bbb149e9ace51
George Edward Swan <geswan@outlook.com>
2019-09-12 16:52:44 +0100

    Formatted unitTest1.cs

--- MapperTests/UnitTest1.cs
+++ MapperTests/UnitTest1.cs
@@ -4,87 +4,80 @@ using System.Globalization;
 
 namespace MapperTests
 {
-	//The [TestClass] attribute must be present in this class
-	//It gives rise to a warning that no test is available in the class
-	//But, without the attribute, tests in the parent class will not be run.
-	[TestClass]
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Unit Test Helper methods")]
-	public class MapperUnitTests : MapperTestsGeneric<ClassA, ClassB>
-	{
-		private readonly Person employeeA = new Person { Name = Constants.PersonAName };
-		private readonly Person employeeB = new Person { Name = Constants.PersonBName };
-		private readonly DateTime classADate = DateTime.Parse(Constants.ClassADate, CultureInfo.CurrentCulture);
-		private readonly DateTime classBDate = DateTime.Parse(Constants.ClassBDate, CultureInfo.CurrentCulture);
+    //The [TestClass] attribute must be present in this class
+    //It gives rise to a warning that no test is available in the class
+    //But, without the attribute, tests in the parent class will not be run.
+    [TestClass]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Unit Test Helper methods")]
+    public class MapperUnitTests : MapperTestsGeneric<ClassA, ClassB>
+    {
+        private readonly Person employeeA = new Person { Name = Constants.PersonAName };
+        private readonly Person employeeB = new Person { Name = Constants.PersonBName };
+        private readonly DateTime classADate = DateTime.Parse(Constants.ClassADate, CultureInfo.CurrentCulture);
+        private readonly DateTime classBDate = DateTime.Parse(Constants.ClassBDate, CultureInfo.CurrentCulture);
 
-		protected override ClassA CreateSampleClassA()
-		{
-			ClassA classA = new ClassA
-			{
-				Name = Constants.NameA,
-				Age = Constants.AgeA,
-				Cash = Constants.CashA,
-				Code = Constants.CodeA,
-                Id=Constants.IdAValue,
-				Date = classADate,
-				Employee = employeeA
-			};
-			return classA;
-		}
+        protected override ClassA CreateSampleClassA()
+        {
+            ClassA classA = new ClassA
+            {
+                Name = Constants.NameA,
+                Age = Constants.AgeA,
+                Cash = Constants.CashA,
+                Code = Constants.CodeA,
+                Id = Constants.IdAValue,
+                Date = classADate,
+                Employee = employeeA
+            };
+            return classA;
+        }
 
-		protected override ClassB CreateSampleClassB()
-		{
-			ClassB classB = new ClassB
-			{
-				Name = Constants.NameB,
-				Age = Constants.AgeB,
-				Cash = Constants.CashB,
-				CodeName = Constants.CodeNameB,
-				Date = classBDate,
-				Employee = employeeB,
-                RecordNumber=Constants.RecordNumberBValue
-			};
-			return classB;
-		}
+        protected override ClassB CreateSampleClassB()
+        {
+            ClassB classB = new ClassB
+            {
+                Name = Constants.NameB,
+                Age = Constants.AgeB,
+                Cash = Constants.CashB,
+                CodeName = Constants.CodeNameB,
+                Date = classBDate,
+                Employee = employeeB,
+                RecordNumber = Constants.RecordNumberBValue
+            };
+            return classB;
+        }
 
-
-		protected override bool AreAllPropsUnchanged(ClassA a) => a.Name == Constants.NameA &&
-				  a.Age == Constants.AgeA &&
-				  a.Cash == Constants.CashA &&
-				  a.Code == Constants.CodeA &&
+        protected override bool AreAllPropsUnchanged(ClassA a) => a.Name == Constants.NameA &&
+                  a.Age == Constants.AgeA &&
+                  a.Cash == Constants.CashA &&
+                  a.Code == Constants.CodeA &&
                   a.Id == Constants.IdAValue &&
-				  a.Date == classADate &&
-				  a.Employee == employeeA;
+                  a.Date == classADate &&
+                  a.Employee == employeeA;
 
-		protected override bool AreAllPropsUnchanged(ClassB b) => b.Name == Constants.NameB &&
-				  b.Age == Constants.AgeB &&
-				  b.Cash == Constants.CashB &&
-				  b.CodeName == Constants.CodeNameB &&
+        protected override bool AreAllPropsUnchanged(ClassB b) => b.Name == Constants.NameB &&
+                  b.Age == Constants.AgeB &&
+                  b.Cash == Constants.CashB &&
+                  b.CodeName == Constants.CodeNameB &&
                   b.RecordNumber == Constants.RecordNumberBValue &&
                   b.Date == classBDate &&
-				  b.Employee == employeeB;
-		protected override bool AreUnsharedPropsUnchanged(ClassB b) => b.CodeName == Constants.CodeNameB &&
-            b.RecordNumber== Constants.RecordNumberBValue;
+                  b.Employee == employeeB;
+        protected override bool AreUnsharedPropsUnchanged(ClassB b) => b.CodeName == Constants.CodeNameB &&
+            b.RecordNumber == Constants.RecordNumberBValue;
 
+        protected override bool AreUnsharedPropsUnchanged(ClassA a) => a.Code == Constants.CodeA &&
+            a.Id == Constants.IdAValue;
 
-		protected override bool AreUnsharedPropsUnchanged(ClassA a) => a.Code == Constants.CodeA &&
-            a.Id==Constants.IdAValue;
+        protected override bool AreSharedPropsInClassAMatchedToThoseInClassB(ClassA a) => a.Name == Constants.NameB &&
+                            a.Age == Constants.AgeB &&
+                            a.Cash == Constants.CashB &&
+                            a.Date == classBDate &&
+                            a.Employee == employeeB;
 
-
-
-		protected override bool AreSharedPropsInClassAMatchedToThoseInClassB(ClassA a) => a.Name == Constants.NameB &&
-							a.Age == Constants.AgeB &&
-							a.Cash == Constants.CashB &&
-							a.Date == classBDate &&
-							a.Employee == employeeB;
-
-
-
-
-		protected override bool AreSharedPropsInClassBMatchedToThoseInClassA(ClassB b) => b.Name == Constants.NameA &&
-							b.Age == Constants.AgeA &&
-							b.Cash == Constants.CashA &&
-							b.Date == classADate &&
-							b.Employee == employeeA;
+        protected override bool AreSharedPropsInClassBMatchedToThoseInClassA(ClassB b) => b.Name == Constants.NameA &&
+                            b.Age == Constants.AgeA &&
+                            b.Cash == Constants.CashA &&
+                            b.Date == classADate &&
+                            b.Employee == employeeA;
         protected override bool ArePairedNamesMappedFromClassAToClassB(ClassB b) => b.CodeName == Constants.CodeA &&
                     b.RecordNumber == Constants.IdAValue;
 
